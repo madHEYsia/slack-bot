6c8b3c82a08fca02e8ca74d6b738542487e47afc
Shubham Madheysia <submiitr07@gmail.com>
2024-08-15 14:45:58 +0530

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 
 module.exports = {
     slackBotToken: process.env.SLACK_BOT_TOKEN,
+    jiraEmail: process.env.JIRA_EMAIL,
+    jiraProj: process.env.JIRA_PROJECT,
     jiraApiToken: process.env.JIRA_API_TOKEN,
     jiraBaseUrl: process.env.JIRA_BASE_URL,
     openaiApiKey: process.env.OPENAI_API_KEY,
     slackSigningSecret: process.env.SLACK_SIGNING_SECRET,
     confidenceThreshold: 0.7,
 };
-            

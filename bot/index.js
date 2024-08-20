82d1a302ded79027975f4d9b786a516a77d88af7
Shubham Madheysia <submiitr07@gmail.com>
2024-08-20 13:53:21 +0530

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -4,9 +4,10 @@ const bodyParser = require('body-parser');
 const { App } = require('@slack/bolt');
 const fs = require('fs');
 const { slackBotToken, slackSigningSecret, confidenceThreshold } = require('./config');
-const { fetchFromKnowledgeBase } = require('./knowledgeBase');
+const knowledgeBase = require('../knowledgeBase.json');
 const { createJiraTicket, deleteJiraTicket } = require('./jira');
-const { fetchAllBlogContent, processBlogs, generateKnowledgeEmbeddings } = require('../Scrapping/process');
+const { fetchAllBlogContent, processBlogs, generateKnowledgeEmbeddings, getEmbedding } = require('../Scrapping/process');
+const { findMostSimilar, summarizeText, analyzeMessageWithOpenAI } = require('./openaiAnalyzer');
 
 // Initialize the Express app
 const expressApp = express();
@@ -31,14 +32,14 @@ expressApp.post('/slack/events', (req, res) => {
 
 // Initialize the Slack Bolt app
 const slackApp = new App({
-  token: slackBotToken,
-  signingSecret: slackSigningSecret
+    token: slackBotToken,
+    signingSecret: slackSigningSecret
 });
 
 const postOnSlack = async (text, say, client, message, prevJiraKey) => {
     const channel = message.channel;
     const prevMsg = message.previous_message;
-    if(prevMsg) {
+    if (prevMsg) {
         const response = await client.chat.update({
             channel,
             ts: Hash[prevMsg.ts]?.ts,
@@ -52,66 +53,102 @@ const postOnSlack = async (text, say, client, message, prevJiraKey) => {
         const { ts } = response;
         Hash[message.ts] = { ts, prevJiraKey };
     }
+};
+
+const analyseMsg = (text, say, client, message) => {
+    getEmbedding(text)
+        .then((queryEmbedding) => {
+            const { mostSimilarEntryId, maxSimilarity } = findMostSimilar(queryEmbedding);
+
+            if (maxSimilarity > 0.75) {  // Adjust threshold as needed
+                const matchingContent = knowledgeBase.find(entry => entry.id == mostSimilarEntryId);
+
+                return summarizeText(matchingContent.content);
+            }
+            return null;
+        })
+        .then((matchingResponse) => {
+            const parsedResponse = matchingResponse?.data?.choices?.[0]?.message?.content;
+            if (parsedResponse) {
+                postOnSlack(parsedResponse, say, client, message);
+                return null;
+            } else {
+                return createJiraTicket(
+                    'Bug reported in Slack',
+                    text,
+                    'appropriate_assignee'  // Replace with the appropriate assignee logic
+                );
+            }
+        })
+        .then((res) => {
+            if (res !== null) {
+                const jiraResponse = res;
+                if (jiraResponse)
+                    postOnSlack(`Jira ticket created: ${jiraResponse.key}.\nTicket url: ${jiraResponse.self}`, say, client, message, jiraResponse.key);
+                else
+                    postOnSlack(`Jira ticket creation failed`, say, client, message);
+            }
+        })
+        .catch((err) => {
+            console.error('Error handling Slack message or JIRA ticket:', error);
+            postOnSlack(`An error occurred while processing your message.`, say);
+        })
 }
 
 // Define message event handler
 slackApp.message(async ({ message, say, client }) => {
     try {
         const text = message?.text || message?.message?.text;
-        const botResponse = message?.message?.bot_id
-        if(!text || botResponse) return;
+        const botResponse = message?.message?.bot_id;
+        const isThreadMsg = message?.thread_ts;
+        if (!text || botResponse || isThreadMsg) return;
 
         console.log("==================");
         console.log("Message ", message);
         console.log("==================");
-    
-        // Step 1: Check the knowledge base
-        const knowledgeBaseResult = fetchFromKnowledgeBase(text);
-    
-        if (knowledgeBaseResult.confidence >= confidenceThreshold) {
-            postOnSlack(knowledgeBaseResult.answer, say, client, message);
-        } else {
-            const jiraResponse = await createJiraTicket(
-              'Bug reported in Slack',
-              text,
-              'appropriate_assignee'  // Replace with the appropriate assignee logic
-            );
-            if(jiraResponse)
-                postOnSlack(`Jira ticket created: ${jiraResponse.key}.\nTicket url: ${jiraResponse.self}`, say, client, message, jiraResponse.key);
-            else 
-                postOnSlack(`Jira ticket creation failed`, say, client, message);
-        }
+
+        analyzeMessageWithOpenAI(text)
+            .then((response) => {
+                const isQuery = response?.data?.choices?.[0]?.message?.content?.includes('Yes');
+                if(isQuery)
+                    analyseMsg(text, say, client, message);
+                else
+                    console.log(`Not a query: ${text}`);
+            })
+            .catch(error => {
+                console.log("Error in catch ", error)
+            })
     } catch (error) {
-        console.error('Error handling Slack message:', error);
+        console.error('Error handling processing msg:', error);
         postOnSlack(`An error occurred while processing your message.`, say);
     }
 });
 
 fetchAllBlogContent()
-.then(() => {
-    return processBlogs();
-})
-.then((knowledgeBase) => {
-   fs.writeFileSync('knowledgeBase.json', JSON.stringify(knowledgeBase, null, 2));
-   return generateKnowledgeEmbeddings(knowledgeBase);
-})
-.then((embeddings) => {
-    // Start both Express and Slack Bolt app on different ports
-    try {
-        // Start the Express server on port 3001
-        const expressPort = process.env.EXPRESS_PORT || 3001;
-        expressApp.listen(expressPort, () => {
-          console.log(`Express server is running on port ${expressPort}`);
-        });
-    
-        // Start the Slack Bolt app on port 3000
-        const slackPort = process.env.SLACK_PORT || 3000;
-        return slackApp.start(slackPort);
-      } catch (error) {
-        console.error('Failed to start Bolt app or Express server:', error);
-      }
-})
-.then(() => {
-    console.log('⚡️ Bolt app is running!');
-});
+    .then(() => {
+        return processBlogs();
+    })
+    .then((knowledgeBase) => {
+        fs.writeFileSync('knowledgeBase.json', JSON.stringify(knowledgeBase, null, 2));
+        return generateKnowledgeEmbeddings(knowledgeBase);
+    })
+    .then((embeddings) => {
+        // Start both Express and Slack Bolt app on different ports
+        try {
+            // Start the Express server on port 3001
+            const expressPort = process.env.EXPRESS_PORT || 3001;
+            expressApp.listen(expressPort, () => {
+                console.log(`Express server is running on port ${expressPort}`);
+            });
+
+            // Start the Slack Bolt app on port 3000
+            const slackPort = process.env.SLACK_PORT || 3000;
+            return slackApp.start(slackPort);
+        } catch (error) {
+            console.error('Failed to start Bolt app or Express server:', error);
+        }
+    })
+    .then(() => {
+        console.log('⚡️ Bolt app is running!');
+    });
 
